5e46cbc4ca3c43d298d4e85b5235d12e324d3fef
minhcong1472 <jinjnguyen147@gmail.com>
2023-02-23 19:01:16 +0700

    login and /static load

--- src/main/java/com/ringme/cms/config/CustomFilter.java
+++ src/main/java/com/ringme/cms/config/CustomFilter.java
@@ -21,20 +21,34 @@ public class CustomFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("da chay vao");
-        if (!request.getRequestURI().matches("/login")){
-            System.out.println(request.getRequestURI());
-            UserSercurity userDetails = (UserSercurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            Set<String> router = userDetails.getRouter();
-            String path = request.getRequestURI();
-            boolean check = false;
-            for (String str : router){
-                if (antPathMatcher.match(str,path)){
-                    check =  true;
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        System.out.println(request.getRequestURI());
+
+//        if (request.getRequestURI().startsWith("/login") || request.getRequestURI().startsWith("/static/**")) {
+//            filterChain.doFilter(request, response); // bỏ qua các trang được liệt kê
+//            return;
+//        }
+//        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() != null) {
+
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if(principal instanceof UserSercurity){
+                if (!request.getRequestURI().matches("/login") ||request.getRequestURI().startsWith("/static/**")
+                        || request.getRequestURI().startsWith("/favicon.ico")){
+                    System.out.println(request.getRequestURI());
+                    UserSercurity userDetails = (UserSercurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                    Set<String> router = userDetails.getRouter();
+                    String path = request.getRequestURI();
+                    boolean check = false;
+                    for (String str : router){
+                        if (antPathMatcher.match(str,path)){
+                            check =  true;
+                        }
+                    }
+                    if (check) filterChain.doFilter(request,response);
+                    else response.sendError(HttpStatus.FORBIDDEN.value(),"You not have access");
                 }
             }
-            if (check) filterChain.doFilter(request,response);
-            else response.sendError(HttpStatus.FORBIDDEN.value(),"You not have access");
-        }
-        else filterChain.doFilter(request,response);
+//        }
+        else  filterChain.doFilter(request,response);
     }
 }
