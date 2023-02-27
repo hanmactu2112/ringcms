ae7f7c72a2e8634912b53e64a249f2ff47a00097
minhcong1472 <jinjnguyen147@gmail.com>
2023-02-27 20:49:03 +0700

    add query for search queue

--- src/main/java/com/ringme/cms/service/QueueServiceImpl.java
+++ src/main/java/com/ringme/cms/service/QueueServiceImpl.java
@@ -45,7 +45,53 @@ public class QueueServiceImpl implements QueueService {
 
     @Override
     public Page<Queue> searchQueueByDepartmentTypeMissionProvince(int pageNo, int pageSize, Long departmentId, String type, Long missionId, String province) {
-        Pageable pageable = PageRequest.of(pageNo -1,pageSize);
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        // one
+        if (departmentId != null && type == null && missionId == null && province == null) {
+            return queueRepository.findAllQueueByDepartment(departmentId, pageable);
+        }
+        if (departmentId == null && type != null && missionId == null && province == null) {
+            return queueRepository.findAllQueueByTypeQueue(type, pageable);
+        }
+        if (departmentId == null && type == null && missionId != null && province == null) {
+            return queueRepository.findAllQueueByMission(missionId, pageable);
+        }
+        if (departmentId == null && type == null && missionId == null && province != null) {
+            return queueRepository.findAllQueueByProvince(province, pageable);
+        }
+
+        // two
+        if (departmentId != null && type != null && missionId == null && province == null) {
+            return queueRepository.findAllQueueByDepartmentAndTypeQueue(departmentId, type, pageable);
+        }
+        if (departmentId != null && type == null && missionId != null && province == null) {
+            return queueRepository.findAllQueueByDepartmentAndMission(departmentId,missionId,pageable);
+        }
+        if (departmentId != null && type == null && missionId == null && province != null) {
+            return queueRepository.findAllQueueByDepartmentProvince(departmentId,province,pageable);
+        }
+        if (departmentId == null && type != null && missionId != null && province == null) {
+            return queueRepository.findAllQueueByTypeQueueAndMission(type,missionId,pageable);
+        }
+        if (departmentId == null && type != null && missionId == null && province != null) {
+            return queueRepository.findAllQueueByTypeQueueAndProvince(type,province,pageable);
+        }
+        if (departmentId == null && type == null && missionId != null && province != null) {
+            return queueRepository.findAllQueueByMissionAndProvince(missionId,province,pageable);
+        }
+
+        if (departmentId != null && type == null && missionId == null && province != null) {
+//            return queueRepository.findAllQueueByDepartmentAndTypeQueue(departmentId,type,pageable);
+        }
+        if (departmentId != null && type == null && missionId == null && province != null) {
+//            return queueRepository.findAllQueueByDepartmentAndTypeQueue(departmentId,type,pageable);
+        }
+        if (departmentId != null && type == null && missionId == null && province != null) {
+//            return queueRepository.findAllQueueByDepartmentAndTypeQueue(departmentId,type,pageable);
+        }
+        if (departmentId != null && type == null && missionId == null && province != null) {
+//            return queueRepository.findAllQueueByDepartmentAndTypeQueue(departmentId,type,pageable);
+        }
 
         return null;
     }
