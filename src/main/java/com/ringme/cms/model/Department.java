b0d35795723dece9fd70052893450f1bb6982776
hanmactu2112 <57985968+hanmactu2112@users.noreply.github.com>
2023-02-27 22:08:45 +0700

    update department

--- src/main/java/com/ringme/cms/model/Department.java
+++ src/main/java/com/ringme/cms/model/Department.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 @ToString
 @AllArgsConstructor
 @Entity
-@Table(name = "mission")
+@Table(name = "department")
 @EntityListeners(AuditingEntityListener.class)
 public class Department extends EntityBase implements Serializable {
     private static final long serialVersionUID = -297553281792804396L;
@@ -27,6 +27,4 @@ public class Department extends EntityBase implements Serializable {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
-
-
 }
